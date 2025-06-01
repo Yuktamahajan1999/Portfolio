bdf822a47a513f0235ae887e759e7f375a6341cf
yukta mahajan <yuktamahajan404@gmail.com>
2025-06-02 00:14:21 +0530

    Updated .gitignore

--- Backend/index.js
+++ Backend/index.js
@@ -1,17 +1,18 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
-import router from './Routers/contactRouters.js';
-import { sendContactEmail } from './controllers/contact.js';
+import router from './Routers/contactRouters.js';  
 
 dotenv.config();
 const app = express();
+
 app.use(cors());
 app.use(express.json());
-app.post('/api/contact', sendContactEmail);
 
-const PORT = process.env.PORT || 8000;
+
 app.use('/api', router);
+
+const PORT = process.env.PORT || 3000;  
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
